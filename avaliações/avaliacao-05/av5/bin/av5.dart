238b2911afb0eb6774761d98d44fa4fa9963b0cf
Miguelito0o <miguel.ribeiro111155@gmail.com>
2025-06-03 11:52:40 -0300

    modificações av5

--- avaliações/avaliacao-05/av5/bin/av5.dart
+++ avaliações/avaliacao-05/av5/bin/av5.dart
@@ -1,17 +1,16 @@
-import 'dart:io';
 import 'package:mailer/mailer.dart';
 import 'package:mailer/smtp_server/gmail.dart';
 
 main() async {
   // Configura as credenciais SMTP do Gmail
-  final smtpServer = gmail('brenno.nogueira09@aluno.ifce.edu.br', 'ucdrfypmdpokrwjs');
+  final smtpServer = gmail('miguel.ribeiro07@aluno.ifce.edu.br', 'epqy hzsd pcvs xqhr');
 
   // Cria uma mensagem de e-mail
   final message = Message()
-    ..from = Address('brenno.nogueira09@aluno.ifce.edu.br', 'lalalla')
-    ..recipients.add('brenno.nogueira09@aluno.ifce.edu.br')
-    ..subject = 'Boa tarde'
-    ..text = 'hoje tem amistoso';
+    ..from = Address('miguel.ribeiro07@aluno.ifce.edu.br', 'Miguelito')
+    ..recipients.add('joaogtito@gmail.com')
+    ..subject = 'Prova Prática 01'
+    ..text = '';
 
   try {
     // Envia o e-mail usando o servidor SMTP do Gmail
